8f40b3c0fa7647a0a06437542ef05a6c4023937c
bottinolucas <lucasgabottino@gmail.com>
2025-02-10 22:51:41 -0300

    add: adicionando questao 312 e foto do submit

---
+++ Questão_312/code_312.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = [1] + nums + [1]
+        n = len(nums)
+        
+        dp = [[0] * n for _ in range(n)]
+        
+        for length in range(2, n): 
+            for i in range(n - length): 
+                j = i + length 
+                for k in range(i + 1, j):
+                    dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j] + nums[i] * nums[k] * nums[j])
+    
+        return dp[0][n-1]
